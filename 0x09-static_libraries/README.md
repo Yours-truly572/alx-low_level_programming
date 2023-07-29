5a0c107cfc046765fa6a1573562e09c55db38731
Yours-truly572 <murimirick01@gmail.com>
2023-07-29 06:49:24 -0700

    Suicide

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,16 @@
+These are C - Static libraries
+
+MY TASKS
+
+
+0. A library is not a luxury but one of the necessities of life(libmy.a)
+
+I have to create the static library libmy.a containing all the functions listed below:
+
+Plus if you haven’t coded all of the above functions create empty ones with the right prototype.
+Don’t forget to push your main.h file to your repository. It should at least contain all the prototypes of the above functions.
+
+1. Without libraries what have we? We have no past and no future create static (lib.sh)
+
+Create a script called create staticlib.sh that creates a static library called liball.a from all the .c files that are in the current directory.
+
---
+++ 0x09-static_libraries/lib/my.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c) {
+    return putchar(c);
+}
+
+int _islower(int c) {
+    return islower(c);
+}
+
+int _isalpha(int c) {
+    return isalpha(c);
+}
+
+int _abs(int n) {
+    return abs(n);
+}
+
+int _isupper(int c) {
+    return isupper(c);
+}
+
+int _isdigit(int c) {
+    return isdigit(c);
+}
+
+int _strlen(char *s) {
+    return strlen(s);
+}
+
+void _puts(char *s) {
+    puts(s);
+}
+
+char *_strcpy(char *dest, char *src) {
+    return strcpy(dest, src);
+}
+
+int _atoi(char *s) {
+    return atoi(s);
+}
+
+char *_strcat(char *dest, char *src) {
+    return strcat(dest, src);
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    return strncat(dest, src, n);
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    return strncpy(dest, src, n);
+}
+
+int _strcmp(char *s1, char *s2) {
+    return strcmp(s1, s2);
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    return memset(s, b, n);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    return memcpy(dest, src,
